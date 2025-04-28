f8372a64903a0cda6231cc43ae93742863ff3b7f
Drew Harris <drew@drewh.net>
2025-04-28 01:34:01 -0500

    drizzle setup

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,12 @@ export const env = createEnv({
   server: {
     BETTER_AUTH_SECRET: z.string(),
     BETTER_AUTH_URL: z.string(),
+    DATABASE_URL: z.string(),
   },
   runtimeEnv: {
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
   emptyStringAsUndefined: true,
 });
