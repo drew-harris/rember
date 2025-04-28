62c16a08eabb2d96f6d7d2f01e82b472461f8228
Drew Harris <drew@drewh.net>
2025-04-28 00:57:50 -0500

    starting better auth

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string(),
+  },
+  runtimeEnv: {
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+  },
+  emptyStringAsUndefined: true,
+});
